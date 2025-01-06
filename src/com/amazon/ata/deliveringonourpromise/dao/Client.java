da094bc6eb19980d4a257fe30f3b276d504edeb9
Kasi David <kasidkure@gmail.com>
2025-01-06 13:40:18 +0100

    Cleared checkstyle and spot bugs

--- src/com/amazon/ata/deliveringonourpromise/dao/Client.java
+++ src/com/amazon/ata/deliveringonourpromise/dao/Client.java
@@ -2,6 +2,14 @@ package com.amazon.ata.deliveringonourpromise.dao;
 
 import com.amazon.ata.deliveringonourpromise.types.Promise;
 
+/**
+ * Client interface to abstract client promises.
+ */
 public interface Client {
+    /**
+     * Get object method to be implemented.
+     * @param customerOrderItemId Order item Id.
+     * @return Object abstracted object.
+     */
     Promise getDeliveryPromiseByOrderItemId(String customerOrderItemId);
 }
--- src/com/amazon/ata/deliveringonourpromise/orderfulfillmentservice/OrderFulfillmentServiceClient.java
+++ src/com/amazon/ata/deliveringonourpromise/orderfulfillmentservice/OrderFulfillmentServiceClient.java
@@ -2,7 +2,6 @@ package com.amazon.ata.deliveringonourpromise.orderfulfillmentservice;
 
 import com.amazon.ata.deliveringonourpromise.dao.Client;
 import com.amazon.ata.deliveringonourpromise.types.Promise;
-import com.amazon.ata.deliverypromiseservice.service.DeliveryPromise;
 import com.amazon.ata.orderfulfillmentservice.OrderFulfillmentService;
 import com.amazon.ata.orderfulfillmentservice.OrderPromise;
 
